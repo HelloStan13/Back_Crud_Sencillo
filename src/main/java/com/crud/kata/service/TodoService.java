94536f71bdc44e40e38a633860eb7f0eb0235d97
Adryan <adryanynfante@gmail.com>
2022-05-04 12:14:30 -0500

    desarrollo del servicio
    servicio listar, guardar, eliminar y consultar por id

--- src/main/java/com/crud/kata/service/TodoService.java
+++ src/main/java/com/crud/kata/service/TodoService.java
@@ -1,4 +1,30 @@
 package com.crud.kata.service;
 
+import com.crud.kata.model.Todo;
+import com.crud.kata.repository.ITodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
+
+    @Autowired
+    private ITodoRepository repository;
+
+    public Iterable<Todo> list(){
+        return repository.findAll();
+    }
+
+    public Todo save(Todo todo){
+        return repository.save(todo);
+    }
+
+    public void delete(Long id){
+        repository.delete(get(id));
+    }
+
+    public Todo get(Long id){
+        return repository.findById(id).orElseThrow();
+    }
+
 }
