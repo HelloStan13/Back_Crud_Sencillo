35c6742232051e92df62746327ef4749484f86da
Adryan <adryanynfante@gmail.com>
2022-05-04 12:05:16 -0500

    creacion back kata
    kata con raul

---
+++ src/main/java/com/crud/kata/KataSencillaApplication.java
@@ -0,0 +1,13 @@
+package com.crud.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataSencillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataSencillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/kata/KataSencillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataSencillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
