100fd89f090a438310ec4059ca92a111e66f2996
Adryan <adryanynfante@gmail.com>
2022-05-04 12:23:20 -0500

    controller
    controller aplicado, cada servicio

--- src/main/java/com/crud/kata/controller/TodoController.java
+++ src/main/java/com/crud/kata/controller/TodoController.java
@@ -1,6 +1,10 @@
 package com.crud.kata.controller;
 
 import com.crud.kata.model.Todo;
+import com.crud.kata.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,5 +12,35 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
 
+    @Autowired
+    private TodoService service;
+
+    @GetMapping(value = "api/todos")
+    public Iterable<Todo> list(){
+        return service.list();
+    }
+
+    @PostMapping(value = "api/todo")
+    public Todo save(@RequestBody Todo todo){
+        return service.save(todo);
+    }
+
+    @PutMapping(value = "api/todo")
+    public Todo update(@RequestBody Todo todo){
+        if(todo.getId() != null){
+            return service.save(todo);
+        }
+        throw new RuntimeException("No existe el id para actualziar");
+    }
+
+    @DeleteMapping(value = "api/{id}/todo")
+    public void delete(@PathVariable("id")Long id){
+        service.delete(id);
+    }
+
+    @GetMapping(value = "api/{id}/todo")
+    public Todo get(@PathVariable("id") Long id){
+        return service.get(id);
+    }
 
 }
